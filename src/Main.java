694d5533280e362760b10b409db597614f36dcea
abcdef-21 <241104@std.hi-joho.ac.jp>
2025-05-09 11:44:53 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
         // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.print("こんにちは。");
-        System.out.println("今日の天気は"+ "\t晴れです。");
-        System.out.print("今日は一日"+"\n"+"洗濯日和です。");
-        System.out.println("外に干しても大丈夫そうです。");
-        System.out.print("文字は"+"\"で囲んでみましょう。");
-
+        System.out.print("林　頼吾");
+        
     }
 }
