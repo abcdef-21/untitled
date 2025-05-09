cb995af436005400e652bc013ee7578c8c51e946
unknown <241104>
2025-05-09 11:37:34 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
+        // IntelliJ IDEA によるその修正案を確認できます。
+        System.out.print("こんにちは。");
+        System.out.println("今日の天気は"+ "\t晴れです。");
+        System.out.print("今日は一日"+"\n"+"洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.print("文字は"+"\"で囲んでみましょう。");
+
+    }
+}
