3755798445a79f01153bcf8fb347a3c153d9a050
hayashi Raigo <241104@std.hi-joho.ac.jp>
2025-05-16 11:24:48 +0900

    復習問題１

---
+++ src/fukusyu1.java
@@ -0,0 +1,9 @@
+public class fukusyu1 {
+    public static void main(String[] args){
+        System.out.print("こんにちは");
+        System.out.println("今日の天気は"+"\t晴れです。");
+        System.out.print("今日は一日"+"\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は"+"\"で囲みましょう。");
+    }
+}
