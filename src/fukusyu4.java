5a1ea8732d90b20d30b73074bab7b3c8d72a4d6a
hayashi Raigo <241104@std.hi-joho.ac.jp>
2025-05-23 11:40:25 +0900

    復習問題4

---
+++ src/fukusyu4.java
@@ -0,0 +1,15 @@
+public class fukusyu4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = "88" + "88";
+        final double PI = 3.141592;
+        double Pi = 3.14;
+        float circ =3.14f;
+        long a = 1000000000;
+        boolean flg =true;
+        byte b = 35;
+        String num = "Ox735";
+        char c = 'a';
+
+    }
+}
