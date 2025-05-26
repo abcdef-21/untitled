3f3df99c85f0d396378f44c2acaa2f22967d357e
hayashi Raigo <241104@std.hi-joho.ac.jp>
2025-05-26 12:29:37 +0900

    復習問題12

--- src/reviews/reviews2/Review15.java
+++ src/reviews/reviews2/Review15.java
@@ -7,6 +7,8 @@ public class Review15 {
             System.out.println("偶数");
         }else if(num % 2 == 1){
             System.out.println("奇数");
+        }else{
+            System.out.println("無効な入力です。");
         }
     }
 
