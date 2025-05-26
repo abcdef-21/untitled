3edb67a20d469d17017ed11cee226ca9c1d6a430
hayashi Raigo <241104@std.hi-joho.ac.jp>
2025-05-26 12:27:55 +0900

    復習問題12

---
+++ src/reviews/reviews1/Review13.java
@@ -0,0 +1,9 @@
+package reviews.reviews1;
+
+public class Review13 {
+    public static void main(String[] args){
+        double randomNum = Math.random()*4;
+        int fortune = (int)randomNum;
+
+    }
+}
---
+++ src/reviews/reviews2/Review15.java
@@ -0,0 +1,13 @@
+package reviews.reviews2;
+
+public class Review15 {
+    public static void main(String[] args){
+        int num = 10;
+        if(num % 2 == 0){
+            System.out.println("偶数");
+        }else if(num % 2 == 1){
+            System.out.println("奇数");
+        }
+    }
+
+}
