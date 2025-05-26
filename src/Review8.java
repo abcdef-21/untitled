c638c2dfda301f944d28e06e7334aad9af408643
hayashi Raigo <241104@std.hi-joho.ac.jp>
2025-05-26 11:25:30 +0900

    復習問題8

---
+++ src/Review8.java
@@ -0,0 +1,16 @@
+public class Review8 {
+    public static  void main(String[] args){
+        int a=10;
+        double b=a;
+        a=(int)b;
+        a=10000;
+        b=10000L;
+        b=(double)a;
+        double f = 5.94;
+        int s = 10;
+
+
+
+
+    }
+}
