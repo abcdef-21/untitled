cf69efd6612e9f74947005903a78b27ad683bf2e
a <241104@std.hi-joho.ac.jp>
2025-06-02 02:42:09 +0900

    練習問題28

---
+++ src/reviews/reviews3/Main.java
@@ -0,0 +1,20 @@
+package reviews.reviews3;
+
+public class Main {
+    public static void main(String[] args) {
+        Review29 ki = new Review29();
+
+        System.out.print("宛先を入力>");
+        String to = ki.readString();
+
+        System.out.print("件名を入力>");
+        String subject = ki.readString();
+
+        System.out.print("本文を入力>");
+        String body = ki.readString();
+
+        System.out.println(to + "に以下のメールを送信しました。");
+        System.out.println("件名: " + subject);
+        System.out.println("本文: " + body);
+    }
+}
